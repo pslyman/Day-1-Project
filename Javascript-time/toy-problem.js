eb11b0ea799397618ea7657d0cb02868cec73f1e
Porter Lyman <Porter Lyman>
2017-01-14 13:09:08 -0700

    toy problem

---
+++ Javascript-time/toy-problem.js
@@ -0,0 +1,13 @@
+// need a function -- returns a custom greeting/ exception of Johnny getting different greeting
+
+function greeting(name) {
+  return 'Hi ' + name;
+}
+
+
+greeting('Gunnar') {
+  if(name === 'Johnny') {
+  return 'Hi you sexy beast ' + name;
+}
+return 'Hi' + name;
+}
